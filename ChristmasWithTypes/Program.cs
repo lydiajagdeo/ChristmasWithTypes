4c92a25fc248fb8a38319ca14dc52aa3248723ba
Lydia Jagdeo <99758270+lydiajagdeo@users.noreply.github.com>
2022-09-05 18:45:37 -0400

    Updated with my changes

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ChristmasWithTypes
 {
@@ -11,13 +11,13 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"Unlimited vacations", "Winning Lottery ticket","Eat and not gain weight" }; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
